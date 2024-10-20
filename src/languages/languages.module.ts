b675c0415e327e80ca1efe49bbac8923a653e16f
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-19 21:06:51 -0600

    [add] Crea el servicio para gestionar idiomas (languages)

--- src/languages/languages.module.ts
+++ src/languages/languages.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { LanguagesService } from './languages.service';
 
-@Module({})
+@Module({
+  providers: [LanguagesService]
+})
 export class LanguagesModule {}
