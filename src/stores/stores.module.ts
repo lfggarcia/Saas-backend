ed28298b8d5005446a8902210f740e8e733384c1
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-20 15:26:05 -0600

    [add] Crea el controlador para gestionar stores

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StoresService } from './stores.service';
+import { StoresController } from './stores.controller';
 
 @Module({
-  providers: [StoresService]
+  providers: [StoresService],
+  controllers: [StoresController]
 })
 export class StoresModule {}
