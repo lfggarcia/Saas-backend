04ed36f97f7179b06eb9e75cca72dd31418700c7
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-20 17:57:11 -0600

    [update] FormFieldValidationsController to use DTOs

--- src/form-field-validations/form-field-validations.controller.ts
+++ src/form-field-validations/form-field-validations.controller.ts
@@ -1,37 +1,34 @@
 import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards } from '@nestjs/common';
 import { FormFieldValidationsService } from './form-field-validations.service';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
+import { CreateFormFieldValidationDto } from './dto/create-form-field-validation.dto/create-form-field-validation.dto';
+import { UpdateFormFieldValidationDto } from './dto/update-form-field-validation.dto/update-form-field-validation.dto';
 
 @Controller('users/apps/:appId/features/:featureId/screens/:screenId/components/:componentId/form-fields/:formFieldId/validations')
-@UseGuards(JwtAuthGuard)  // Protegemos todas las rutas con autenticación JWT
+@UseGuards(JwtAuthGuard)
 export class FormFieldValidationsController {
   constructor(private readonly formFieldValidationsService: FormFieldValidationsService) {}
 
-  // Obtener todas las validaciones de un campo de formulario (acceso para todos los usuarios autenticados)
   @Get()
   getAllFormFieldValidations(@Param('formFieldId') formFieldId: string) {
     return this.formFieldValidationsService.getAllFormFieldValidations(formFieldId);
   }
 
-  // Obtener una validación específica (acceso para todos los usuarios autenticados)
   @Get(':id')
   getFormFieldValidationById(@Param('id') id: string) {
     return this.formFieldValidationsService.getFormFieldValidationById(id);
   }
 
-  // Crear una nueva validación para un campo de formulario (solo super admin o el dueño de la aplicación)
   @Post()
-  createFormFieldValidation(@Param('formFieldId') formFieldId: string, @Body() createFormFieldValidationDto: any) {
+  createFormFieldValidation(@Param('formFieldId') formFieldId: string, @Body() createFormFieldValidationDto: CreateFormFieldValidationDto) {
     return this.formFieldValidationsService.createFormFieldValidation(formFieldId, createFormFieldValidationDto);
   }
 
-  // Actualizar una validación existente (solo super admin o el dueño de la aplicación)
   @Put(':id')
-  updateFormFieldValidation(@Param('id') id: string, @Body() updateFormFieldValidationDto: any) {
+  updateFormFieldValidation(@Param('id') id: string, @Body() updateFormFieldValidationDto: UpdateFormFieldValidationDto) {
     return this.formFieldValidationsService.updateFormFieldValidation(id, updateFormFieldValidationDto);
   }
 
-  // Eliminar una validación existente (solo super admin o el dueño de la aplicación)
   @Delete(':id')
   deleteFormFieldValidation(@Param('id') id: string) {
     return this.formFieldValidationsService.deleteFormFieldValidation(id);
