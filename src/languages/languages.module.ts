6e75978fdd1e116e58073e338f84e45354d6fdd9
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-19 21:06:52 -0600

    [add] Crea el controlador para gestionar idiomas (languages)

--- src/languages/languages.module.ts
+++ src/languages/languages.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { LanguagesService } from './languages.service';
+import { LanguagesController } from './languages.controller';
 
 @Module({
-  providers: [LanguagesService]
+  providers: [LanguagesService],
+  controllers: [LanguagesController]
 })
 export class LanguagesModule {}
