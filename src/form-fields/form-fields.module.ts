e051974f1af2c80e99f41279456ddfb0dbb7c118
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-20 15:36:40 -0600

    [add] Crea el controlador para gestionar campos de formulario (form_fields)

--- src/form-fields/form-fields.module.ts
+++ src/form-fields/form-fields.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FormFieldsService } from './form-fields.service';
+import { FormFieldsController } from './form-fields.controller';
 
 @Module({
-  providers: [FormFieldsService]
+  providers: [FormFieldsService],
+  controllers: [FormFieldsController]
 })
 export class FormFieldsModule {}
