e573f59535bab9df42dcbdf287bedd4e0b00b3a3
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-19 21:12:02 -0600

    [add] Crea el controlador para gestionar traducciones (translations)

--- src/translations/translations.module.ts
+++ src/translations/translations.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TranslationsService } from './translations.service';
+import { TranslationsController } from './translations.controller';
 
 @Module({
-  providers: [TranslationsService]
+  providers: [TranslationsService],
+  controllers: [TranslationsController]
 })
 export class TranslationsModule {}
