14348cb2e394fc86a17d331e3492c2851ae5bbd6
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-20 21:05:47 -0600

    [update] Ajuste en el módulo de features para incluir la entidad Plan

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -1,13 +1,15 @@
+// features.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeaturesService } from './features.service';
 import { FeaturesController } from './features.controller';
 import { Feature } from '../entities/feature.entity';
 import { Application } from '../entities/application.entity';
+import { Plan } from '../entities/plan.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Feature, Application])],  // Registramos las entidades
-  providers: [FeaturesService],  // Incluimos el servicio
-  controllers: [FeaturesController],  // Incluimos el controlador
+  imports: [TypeOrmModule.forFeature([Feature, Application, Plan])],
+  providers: [FeaturesService],
+  controllers: [FeaturesController],
 })
 export class FeaturesModule {}
