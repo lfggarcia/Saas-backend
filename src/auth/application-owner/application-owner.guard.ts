c9dc6ff3d0b4ddc27b55f03124d9483b3820b067
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-20 18:35:58 -0600

    [add] Guard para verificar la propiedad de las aplicaciones y permitir acceso solo a dueños o superadmin.

--- src/auth/application-owner/application-owner.guard.ts
+++ src/auth/application-owner/application-owner.guard.ts
@@ -1,11 +1,29 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class ApplicationOwnerGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly usersService: UsersService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+    const appId = request.params.appId;
+
+    const application = await this.usersService.getAppById(appId);
+
+    if (!application) {
+      throw new ForbiddenException('Application not found.');
+    }
+
+    if (application.user.id === user.id || user.role === 'superadmin') {
+      return true;
+    }
+
+    throw new ForbiddenException('You do not have permission to manage this application.');
   }
 }
