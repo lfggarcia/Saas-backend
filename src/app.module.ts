10e973538a2c89cc5d6d66def3b91fd74f2e468a
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-22 18:10:03 -0600

    Generated Catalogs module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatalogsModule } from './catalogs/catalogs.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-		TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost,
-      port: process.env.DB_PORT || 5432,
-      username: process.env.DB_USERNAME || 'user', // Cambia esto si es necesario
-      password: process.env.DB_PASSWORD || 'pass', // Cambia esto si es necesario
-      database: process.env.DB_NAME || 'db', // Cambia esto si es necesario
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // No usar en producción
+		ConfigModule.forRoot({
+      isGlobal: true,
     }),
-		CatalogsModule
+    // Configuración de TypeORM
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT', '5432')),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+				entities:[],
+        synchronize: false, // Usar solo en desarrollo
+      }),
+      inject: [ConfigService],
+    }),
+    CatalogsModule,
 	],
   controllers: [AppController],
   providers: [AppService],
