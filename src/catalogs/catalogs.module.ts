d100a2a7c801d90974789752fa99d803960b7a07
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-22 21:41:56 -0600

    feat(catalogs): add TypeOrmModule imports to CatalogsModule

--- src/catalogs/catalogs.module.ts
+++ src/catalogs/catalogs.module.ts
@@ -1,4 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Alias } from './entities/alias.entity';
+import { TokenGroup } from './entities/token-group.entity';
+import { TokenGroupAlias } from './entities/token-group-alias.entity';
+import { DefaultToken } from './entities/default-token.entity';
+import { ComponentType } from './entities/component-type.entity';
+import { FieldType } from './entities/field-type.entity';
+import { ValidationType } from './entities/validation-type.entity';
+import { Status } from './entities/status.entity';
+import { Role } from './entities/role.entity';
+import { Plan } from './entities/plan.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Alias,
+      TokenGroup,
+      TokenGroupAlias,
+      DefaultToken,
+      ComponentType,
+      FieldType,
+      ValidationType,
+      Status,
+      Role,
+      Plan,
+    ]),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
 export class CatalogsModule {}
