f919cfe3c59c76da56ecd88cb4154453785b2cd9
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-24 13:18:28 -0600

    feat(auth): implement AuthService with validateUser and login methods

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService, AuthService]
 })
 export class AuthModule {}
--- src/auth/users.service.ts
+++ src/auth/users.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findOneByEmail(email: string): Promise<User> {
+    return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async create(userData: Partial<User>): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+    const user = this.usersRepository.create({
+      ...userData,
+      password: hashedPassword,
+    });
+
+    return this.usersRepository.save(user);
+  }
+}
