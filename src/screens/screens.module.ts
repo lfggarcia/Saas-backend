72f085dbede7466c5e78b893f5c3cd9ab18edf57
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-24 22:01:43 -0600

    feat(screens): update ScreensModule with necessary imports

--- src/screens/screens.module.ts
+++ src/screens/screens.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ScreensService } from './screens.service';
 import { ScreensController } from './screens.controller';
+import { Screen } from './entities/screen.entity';
+import { Feature } from '../features/entities/feature.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Screen, Feature])],
   providers: [ScreensService],
-  controllers: [ScreensController]
+  controllers: [ScreensController],
 })
 export class ScreensModule {}
