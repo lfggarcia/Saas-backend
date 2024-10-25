0ff71654c7ac736b2be7da0070d8d5e1c421144e
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-24 21:54:35 -0600

    feat(screens): create Screen entity

---
+++ src/screens/entities/screen.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Feature } from '../../features/entities/feature.entity';
+
+@Entity()
+export class Screen {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @ManyToOne(() => Feature, feature => feature.screens)
+  feature: Feature;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  updated_at: Date;
+}
