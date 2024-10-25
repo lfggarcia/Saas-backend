ade5def32eb169237657008674f00aeee8bd9a31
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-24 20:36:04 -0600

    feat(users): create CreateUserDto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { IsString, IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  role_id?: string;
+}
