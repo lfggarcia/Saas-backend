031869adcbf3ac00dee95b7c954dd6dc03618c35
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-04 18:34:20 -0600

    feat(custom-tokens): update CustomTokensModule with necessary imports

--- src/custom-tokens/custom-tokens.module.ts
+++ src/custom-tokens/custom-tokens.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CustomTokensService } from './custom-tokens.service';
 import { CustomTokensController } from './custom-tokens.controller';
+import { CustomToken } from './entities/custom-token.entity';
+import { Theme } from '../themes/entities/theme.entity';
+import { User } from '../users/entities/user.entity';
+import { TokenGroup } from '../catalogs/entities/token-group.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CustomToken, Theme, User, TokenGroup])],
   providers: [CustomTokensService],
-  controllers: [CustomTokensController]
+  controllers: [CustomTokensController],
 })
 export class CustomTokensModule {}
