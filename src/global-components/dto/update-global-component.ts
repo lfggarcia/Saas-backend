fde7a7a14869aa0581a1f0e57eb8a98dcaa4f764
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-04 18:44:22 -0600

    feat(global-components): create UpdateGlobalComponentDto

---
+++ src/global-components/dto/update-global-component.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateGlobalComponentDto } from './create-global-component.dto';
+
+export class UpdateGlobalComponentDto extends PartialType(CreateGlobalComponentDto) {}
