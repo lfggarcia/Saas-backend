d85177dea538c2397be28375307eded050e7a1b0
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-04 18:30:49 -0600

    feat(custom-tokens): create UpdateCustomTokenDto

---
+++ src/custom-tokens/dto/update-custom-token.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCustomTokenDto } from './create-custom-token.dto';
+
+export class UpdateCustomTokenDto extends PartialType(CreateCustomTokenDto) {}
