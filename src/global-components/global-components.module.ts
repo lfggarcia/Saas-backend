de097ee7f10a1dfdeca37c0c31c238753fd2294f
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-04 18:46:13 -0600

    feat(global-components): update GlobalComponentsModule with necessary imports

--- src/global-components/global-components.module.ts
+++ src/global-components/global-components.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { GlobalComponentsService } from './global-components.service';
 import { GlobalComponentsController } from './global-components.controller';
+import { GlobalComponent } from './entities/global-component.entity';
+import { ComponentType } from '../catalogs/entities/component-type.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GlobalComponent, ComponentType, User])],
   providers: [GlobalComponentsService],
-  controllers: [GlobalComponentsController]
+  controllers: [GlobalComponentsController],
 })
 export class GlobalComponentsModule {}
