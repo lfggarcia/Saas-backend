70a578f1cab76c0fef47cce103496be163a12aca
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-07 18:49:19 -0600

    fix(global-components): update module imports

--- src/global-components/global-components.module.ts
+++ src/global-components/global-components.module.ts
@@ -4,11 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GlobalComponentsService } from './global-components.service';
 import { GlobalComponentsController } from './global-components.controller';
 import { GlobalComponent } from './entities/global-component.entity';
-import { App } from '../apps/entities/app.entity';
 import { ComponentType } from '../catalogs/entities/component-type.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GlobalComponent, App, ComponentType])],
+  imports: [TypeOrmModule.forFeature([GlobalComponent, ComponentType]), AuthModule],
   providers: [GlobalComponentsService],
   controllers: [GlobalComponentsController],
 })
