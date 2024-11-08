823d0170a701231d5669f58247774feaa1ef4ecf
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-07 18:30:54 -0600

    feat(global-components): create GlobalComponent entity

--- src/global-components/entities/global-component.entity.ts
+++ src/global-components/entities/global-component.entity.ts
@@ -1,16 +1,33 @@
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
 import { ComponentType } from '../../catalogs/entities/component-type.entity';
+import { App } from '../../apps/entities/app.entity';
+import { ScreenComponent } from '../../screen-components/entities/screen-component.entity';
 
 @Entity()
 export class GlobalComponent {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  name: string;
+
   @ManyToOne(() => ComponentType)
   componentType: ComponentType;
 
+  @ManyToOne(() => App, (app) => app.globalComponents)
+  application: App;
+
   @Column({ type: 'jsonb', nullable: true })
-  defaultProps: any;
+  default_props: Record<string, any>;
+
+  @OneToMany(() => ScreenComponent, (screenComponent) => screenComponent.globalComponent)
+  screenComponents: ScreenComponent[];
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   created_at: Date;
