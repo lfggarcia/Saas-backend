4dab4b3b58e75c4c6fc1ee5e5829c6f8a5f23bfc
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-07 19:17:53 -0600

    feat(feature-versions): create FeatureVersion entity

--- src/feature-versions/entities/feature-version.entity.ts
+++ src/feature-versions/entities/feature-version.entity.ts
@@ -1,1 +1,40 @@
-export class FeatureVersionEntity {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+  JoinColumn,
+} from 'typeorm';
+import { Feature } from '../../features/entities/feature.entity';
+import { Screen } from '../../screens/entities/screen.entity';
+
+@Entity('feature_versions')
+export class FeatureVersion {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Feature, (feature) => feature.featureVersions)
+  @JoinColumn({ name: 'feature_id' })
+  feature: Feature;
+
+  @Column()
+  version: string;
+
+  @ManyToOne(() => FeatureVersion, { nullable: true })
+  @JoinColumn({ name: 'replaces_feature_version_id' })
+  replacesFeatureVersion: FeatureVersion;
+
+  @OneToMany(() => Screen, (screen) => screen.featureVersion)
+  screens: Screen[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+
+  @Column({
+    type: 'timestamp',
+    nullable: true,
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updated_at: Date;
+}
