c2cfb8099cca0e328688bbcf612fa594dd6ebbe4
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-09 23:44:44 -0600

    Refactor UpdateThemeTokenDto to include tokenGroup property

--- src/theme-tokens/dto/update-theme-token.dto.ts
+++ src/theme-tokens/dto/update-theme-token.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateThemeTokenDto } from './create-theme-token.dto';
 
-export class UpdateThemeTokenDto extends PartialType(CreateThemeTokenDto) {}
+export class UpdateThemeTokenDto extends PartialType(CreateThemeTokenDto) {
+	tokenGroup?: { id: string; };
+}
