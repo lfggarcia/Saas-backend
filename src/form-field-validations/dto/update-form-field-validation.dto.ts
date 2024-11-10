9041d018b1a56a9a0dc46d0ed8758b8732007c2a
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-09 23:42:26 -0600

    Refactor UpdateFormFieldValidationDto to include validationType property

--- src/form-field-validations/dto/update-form-field-validation.dto.ts
+++ src/form-field-validations/dto/update-form-field-validation.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateFormFieldValidationDto } from './create-form-field-validation.dto';
 
-export class UpdateFormFieldValidationDto extends PartialType(CreateFormFieldValidationDto) {}
+export class UpdateFormFieldValidationDto extends PartialType(CreateFormFieldValidationDto) {
+	validationType?: { id: string; };
+}
