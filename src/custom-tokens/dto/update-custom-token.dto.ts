30ab0b65de8ff1cac3c0686851eba5f6a2315741
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-09 23:41:51 -0600

    feat(custom-tokens): add tokenGroup property to UpdateCustomTokenDto

--- src/custom-tokens/dto/update-custom-token.dto.ts
+++ src/custom-tokens/dto/update-custom-token.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCustomTokenDto } from './create-custom-token.dto';
 
-export class UpdateCustomTokenDto extends PartialType(CreateCustomTokenDto) {}
+export class UpdateCustomTokenDto extends PartialType(CreateCustomTokenDto) {
+	tokenGroup?: { id?: string; };
+}
