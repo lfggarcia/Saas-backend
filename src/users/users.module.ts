5d69037e6b819bd5e649667883262213478cc2aa
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-09 23:44:49 -0600

    Refactor users module to include Role entity in imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { Role } from '../catalogs/entities/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User,Role])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
