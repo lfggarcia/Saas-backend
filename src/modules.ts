7505fe56705795916222d59ad86d7d48ab203767
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-10 01:00:10 -0600

    Refactor modules.ts: Add all module imports

---
+++ src/modules.ts
@@ -0,0 +1,47 @@
+import { AppsModule } from "./apps/apps.module"
+import { AuthModule } from "./auth/auth.module"
+import { CatalogsModule } from "./catalogs/catalogs.module"
+import { ComponentsModule } from "./components/components.module"
+import { CustomTokensModule } from "./custom-tokens/custom-tokens.module"
+import { FeatureVersionsModule } from "./feature-versions/feature-versions.module"
+import { FeaturesModule } from "./features/features.module"
+import { FormFieldValidationsModule } from "./form-field-validations/form-field-validations.module"
+import { FormFieldsModule } from "./form-fields/form-fields.module"
+import { GlobalComponentsModule } from "./global-components/global-components.module"
+import { GlobalStyleVariantsModule } from "./global-style-variants/global-style-variants.module"
+import { GlobalStylesModule } from "./global-styles/global-styles.module"
+import { I18nModule } from "./i18n/i18n.module"
+import { ReducerActionsModule } from "./reducer-actions/reducer-actions.module"
+import { ReducersModule } from "./reducers/reducers.module"
+import { ScreenComponentsModule } from "./screen-components/screen-components.module"
+import { ScreenVersionsModule } from "./screen-versions/screen-versions.module"
+import { ScreensModule } from "./screens/screens.module"
+import { StoresModule } from "./stores/stores.module"
+import { ThemeTokensModule } from "./theme-tokens/theme-tokens.module"
+import { ThemesModule } from "./themes/themes.module"
+import { UsersModule } from "./users/users.module"
+
+export default [
+	AppsModule,
+	AuthModule,
+	CatalogsModule,
+	ComponentsModule,
+	CustomTokensModule,
+	FeatureVersionsModule,
+	FeaturesModule,
+	FormFieldValidationsModule,
+	FormFieldsModule,
+	GlobalComponentsModule,
+	GlobalStyleVariantsModule,
+	GlobalStylesModule,
+	I18nModule,
+	ReducerActionsModule,
+	ReducersModule,
+	ScreenComponentsModule,
+	ScreenVersionsModule,
+	ScreensModule,
+	StoresModule,
+	ThemeTokensModule,
+	ThemesModule,
+	UsersModule
+]
