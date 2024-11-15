c22dabb7e049731d3549845a783715e717b7420d
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-14 22:38:11 -0600

    Refactor entity import in TestService

--- src/test/test.service.ts
+++ src/test/test.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { TranslationValues } from '../entities';
+import { Users } from '../entities';
 
 @Injectable()
 export class TestService {
   constructor(
-    @InjectRepository(TranslationValues)
-    private aliasesRepository: Repository<TranslationValues>,
+    @InjectRepository(Users)
+    private aliasesRepository: Repository<Users>,
   ) {}
 
   async findAll() {
