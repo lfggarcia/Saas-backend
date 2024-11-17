9f1090b1f716ccbf2694d7d0c3023d9e09a328f4
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-16 22:39:10 -0600

    Add FieldTypes module with controller and service to manage field types

--- src/modules/catalogs/catalogs.module.ts
+++ src/modules/catalogs/catalogs.module.ts
@@ -2,32 +2,37 @@ import { Module } from '@nestjs/common';
 import { AliasesService } from './aliases/aliases.service';
 import { AliasesController } from './aliases/aliases.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Aliases, ComponentTypes, DefaultTokens, TokenGroups } from '../../entities';
+import { Aliases, ComponentTypes, DefaultTokens, FieldTypes, TokenGroups } from '../../entities';
 import { TokenGroupsService } from './token-groups/token-groups.service';
 import { TokenGroupsController } from './token-groups/token-groups.controller';
 import { DefaultTokensService } from './default-tokens/default-tokens.service';
 import { DefaultTokensController } from './default-tokens/default-tokens.controller';
 import { ComponentTypesController } from './component-types/component-types.controller';
 import { ComponentTypesService } from './component-types/component-types.service';
+import { FieldTypesService } from './field-types/field-types.service';
+import { FieldTypesController } from './field-types/field-types.controller';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([
 		Aliases,
 		TokenGroups,
 		DefaultTokens,
-		ComponentTypes
+		ComponentTypes,
+		FieldTypes
 	])],
   providers: [
 		AliasesService,
 		TokenGroupsService,
 		DefaultTokensService,
-		ComponentTypesService
+		ComponentTypesService,
+		FieldTypesService
 	],
   controllers: [
 		AliasesController,
 		TokenGroupsController,
 		DefaultTokensController,
-		ComponentTypesController
+		ComponentTypesController,
+		FieldTypesController
 	]
 })
 export class CatalogsModule {}
