e8353f027efbb033d313bfbc83a6376dd2a6ed72
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-16 22:27:10 -0600

    Add DefaultTokens module with controller and service for managing default tokens

---
+++ src/modules/catalogs/default-tokens/default-tokens.controller.ts
@@ -0,0 +1,41 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/commons/guards/roles.guard';
+import { DefaultTokensService } from './default-tokens.service';
+import { Roles } from 'src/commons/decorators/roles.decorator';
+
+@Controller('catalogs')
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+export class DefaultTokensController {
+	constructor(
+		private readonly defaultTokensService: DefaultTokensService
+	) {}
+
+	@Get('default-tokens')
+	async findAll(@Query() query: any) {
+		return this.defaultTokensService.findAll(query);
+	}
+
+	@Get('default-tokens/:id')
+	async findOne(id: string) {
+		return this.defaultTokensService.findOne(id);
+	}
+
+	@Post('default-tokens')
+	@Roles('admin')
+	async create(@Query() query: any) {
+		return this.defaultTokensService.create(query);
+	}
+
+	@Patch('default-tokens/:id')
+	@Roles('admin')
+	async update(@Param('id') id: string, @Body() data: any) {
+		return this.defaultTokensService.update(id, data);
+	}
+
+	@Delete('default-tokens/:id')
+	@Roles('admin')
+	async remove(@Param('id') id: string) {
+		return this.defaultTokensService.remove(id);
+	}
+}
