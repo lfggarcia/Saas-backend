43e1fcb82467c317c159129c864b533f8a03156e
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-16 21:14:39 -0600

    Refactor LocalStrategy to clarify username field usage

--- src/commons/strategies/local.strategy.ts
+++ src/commons/strategies/local.strategy.ts
@@ -7,7 +7,7 @@ import { AuthService } from '../../modules/auth/auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super({ usernameField: 'email' }); // Usamos 'email' en lugar de 'username'
+    super({ usernameField: 'email' });
   }
 
   async validate(email: string, password: string): Promise<any> {
