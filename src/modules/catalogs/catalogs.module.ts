baf303afc3a76bd570cbf286ae98748d291a175d
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-11-16 22:27:40 -0600

    Add DefaultTokens service and controller to Catalogs module

--- src/modules/catalogs/catalogs.module.ts
+++ src/modules/catalogs/catalogs.module.ts
@@ -2,13 +2,27 @@ import { Module } from '@nestjs/common';
 import { AliasesService } from './aliases/aliases.service';
 import { AliasesController } from './aliases/aliases.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Aliases, TokenGroups } from '../../entities';
+import { Aliases, DefaultTokens, TokenGroups } from '../../entities';
 import { TokenGroupsService } from './token-groups/token-groups.service';
 import { TokenGroupsController } from './token-groups/token-groups.controller';
+import { DefaultTokensService } from './default-tokens/default-tokens.service';
+import { DefaultTokensController } from './default-tokens/default-tokens.controller';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([Aliases,TokenGroups])],
-  providers: [AliasesService, TokenGroupsService],
-  controllers: [AliasesController, TokenGroupsController]
+	imports: [TypeOrmModule.forFeature([
+		Aliases,
+		TokenGroups,
+		DefaultTokens
+	])],
+  providers: [
+		AliasesService,
+		TokenGroupsService,
+		DefaultTokensService
+	],
+  controllers: [
+		AliasesController,
+		TokenGroupsController,
+		DefaultTokensController
+	]
 })
 export class CatalogsModule {}
