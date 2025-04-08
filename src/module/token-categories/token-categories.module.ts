295a24432479b333b6a0acc931319724bb7afedd
Luis Garcia <luis.garciasvv@gmail.com>
2025-04-08 17:20:38 -0600

    add entity to module

--- src/module/token-categories/token-categories.module.ts
+++ src/module/token-categories/token-categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TokenCategoriesService } from './token-categories.service';
 import { TokenCategoriesController } from './token-categories.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TokenCategories } from '../../entities';
 
 @Module({
+	imports: [TypeOrmModule.forFeature([TokenCategories])],
   controllers: [TokenCategoriesController],
   providers: [TokenCategoriesService],
 })
