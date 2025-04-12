4ed0ab94165e14bde943a8d4f0641aad79de099a
Luis Garcia <luis.garciasvv@gmail.com>
2025-04-11 21:42:24 -0600

    refactor(permission-type-catalog): enhance findAll and remove methods to accept query parameters and id as string

--- src/module/permission_type_catalog/permission_type_catalog.controller.ts
+++ src/module/permission_type_catalog/permission_type_catalog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { PermissionTypeCatalogService } from './permission_type_catalog.service';
 import { CreatePermissionTypeCatalogDto } from './dto/create-permission_type_catalog.dto';
 import { UpdatePermissionTypeCatalogDto } from './dto/update-permission_type_catalog.dto';
@@ -13,22 +13,22 @@ export class PermissionTypeCatalogController {
   }
 
   @Get()
-  findAll() {
-    return this.permissionTypeCatalogService.findAll();
+  findAll(@Query() query: Partial<CreatePermissionTypeCatalogDto>) {
+    return this.permissionTypeCatalogService.findAll(query);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.permissionTypeCatalogService.findOne(+id);
+    return this.permissionTypeCatalogService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePermissionTypeCatalogDto: UpdatePermissionTypeCatalogDto) {
-    return this.permissionTypeCatalogService.update(+id, updatePermissionTypeCatalogDto);
+    return this.permissionTypeCatalogService.update(id, updatePermissionTypeCatalogDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.permissionTypeCatalogService.remove(+id);
+    return this.permissionTypeCatalogService.remove(id);
   }
 }
