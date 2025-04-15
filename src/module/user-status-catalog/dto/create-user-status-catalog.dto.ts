70c946f684b01d2e7aaeeb9f0843fb1c49d8e1e8
Luis Garcia <luisfernando.garcia@inswitch.com>
2025-04-15 13:53:26 -0600

    feat(user-status-catalog): enhance CreateUserStatusCatalogDto with validation and transformation decorators

--- src/module/user-status-catalog/dto/create-user-status-catalog.dto.ts
+++ src/module/user-status-catalog/dto/create-user-status-catalog.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserStatusCatalogDto {}
+import { Expose, Type } from "class-transformer";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserStatusCatalogDto {
+	@Expose()
+	@IsString()
+	@IsNotEmpty()
+	@Type(() => String)
+	name: string;
+
+	@Expose()
+	@IsString()
+	@IsNotEmpty()
+	@Type(() => String)
+	description: string;
+}
